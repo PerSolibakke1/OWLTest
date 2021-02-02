c6ee0b5735a75d4489eca62f6171964e5673cdb2
Per Solibakke <persol@stud.ntnu.no>
2021-02-02 11:11:06 +0100

    Hold Up

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const fetch = require('node-fetch')
 const app = express()
 const port = 3000
 
-const url = 'http://localhost:7200/repositories/wineTest?query=PREFIX%20%3A%20%3Chttp%3A%2F%2Fwww.w3.org%2FTR%2F2003%2FPR-owl-guide-20031209%2Fwine%23%3E%0ASELECT%20%3FWine%20%3FMaker%0AWHERE%20%7B%0A%09%3FWine%20%3AhasBody%20%3AFull%20.%0A%20%20%20%20%3FWine%20%3AhasMaker%20%3FMaker%20.%0A%7D%20LIMIT%20100';
+const url = 'http://localhost:7200/test';
 
 app.get('/', async (req, res) => {
     const data = await fetch(url, {
