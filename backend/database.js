4cb1f44ef14127652bfa088929e0e27b16e1889e
AndreasWintherMoen <andreas@winthermoen.no>
2021-02-02 17:14:05 +0100

    Create database.js for DB queries and credentials

---
+++ backend/database.js
@@ -0,0 +1,90 @@
+// const { EnapsoGraphDBClient } = require("@innotrade/enapso-graphdb-client");
+import { EnapsoGraphDBClient } from '@innotrade/enapso-graphdb-client';
+
+
+const GRAPHDB_BASE_URL = "http://localhost:7200",
+GRAPHDB_REPOSITORY = "Node1",
+GRAPHDB_USERNAME = "Node",
+GRAPHDB_PASSWORD = "Node",
+GRAPHDB_CONTEXT_TEST = "http://ont.enapso.com/repo";
+
+const DEFAULT_PREFIXES = [
+    EnapsoGraphDBClient.PREFIX_OWL,
+    EnapsoGraphDBClient.PREFIX_RDF,
+    EnapsoGraphDBClient.PREFIX_RDFS,
+    EnapsoGraphDBClient.PREFIX_XSD,
+    EnapsoGraphDBClient.PREFIX_PROTONS,
+    {
+        prefix: "Testing",
+        iri: "http://ont.enapso.com/Node1#",
+    }
+];
+
+const graphDBEndpoint = new EnapsoGraphDBClient.Endpoint({
+    baseURL: GRAPHDB_BASE_URL,
+    repository: GRAPHDB_REPOSITORY,
+    prefixes: DEFAULT_PREFIXES
+});
+
+export const login = () => {
+    graphDBEndpoint.login(GRAPHDB_USERNAME,GRAPHDB_PASSWORD)
+        .then((result) => {
+            console.log(result);
+        }).catch((err) => {
+            console.log(err);
+        });
+}
+
+export const readClass = (className) => {
+    const query = `
+        select *
+        where {
+            ?class rdf:type owl:Class
+            filter(regex(str(?class), "Wine", "i")) .
+        }`;
+
+    graphDBEndpoint
+        .query(query, { transform: "toJSON" })
+        .then((result) => {
+            console.log("Read a class:\n" + JSON.stringify(result, null, 2));
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+}
+
+export const readAllClasses = () => {
+    const query = `
+        select ?class
+        where {
+            ?class a owl:Class .
+        }`;
+
+    graphDBEndpoint
+        .query(query, { transform: "toJSON" })
+        .then((result) => {
+            console.log("Read all class in graph :\n" + JSON.stringify(result, null, 2));
+        })
+        .catch((err) => {
+            console.log('error reading all classes')
+            console.log(err);
+        });
+}
+
+export const insertClass = () => {
+    const query = `
+        insert data {
+            graph <${GRAPHDB_CONTEXT_TEST}> {
+                Testing:Hello rdf:type owl:Class
+            }
+        }`;
+
+    graphDBEndpoint
+        .update(query)
+        .then((result) => {
+            console.log("inserted a class :\n" + JSON.stringify(result, null, 2));
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+}
--- backend/index.js
+++ backend/index.js
@@ -1,79 +1,15 @@
+import { login, readClass, readAllClasses, insertClass } from './database.js';
+import express from 'express';
 
-const {
-    EnapsoGraphDBClient } = require("@innotrade/enapso-graphdb-client");
-// connection data to the run GraphDB instance
-const GRAPHDB_BASE_URL = "http://localhost:7200",
-    GRAPHDB_REPOSITORY = "Node1",
-    GRAPHDB_USERNAME = "Node",
-    GRAPHDB_PASSWORD = "Node",
-    GRAPHDB_CONTEXT_TEST = "http://ont.enapso.com/repo";
-const DEFAULT_PREFIXES = [
-    EnapsoGraphDBClient.PREFIX_OWL,
-    EnapsoGraphDBClient.PREFIX_RDF,
-    EnapsoGraphDBClient.PREFIX_RDFS,
-    EnapsoGraphDBClient.PREFIX_XSD,
-    EnapsoGraphDBClient.PREFIX_PROTONS,
-    {
-        prefix: "Testing",
-        iri: "http://ont.enapso.com/Node1#",
-    }
-];
+const app = express()
+const port = 3000
 
-let graphDBEndpoint = new EnapsoGraphDBClient.Endpoint({
-    baseURL: GRAPHDB_BASE_URL,
-    repository: GRAPHDB_REPOSITORY,
-    prefixes: DEFAULT_PREFIXES
-});
-graphDBEndpoint.login(GRAPHDB_USERNAME,GRAPHDB_PASSWORD)
-.then((result) => {
-    console.log(result);
-}).catch((err) => {
-    console.log(err);
-});
-//insert a class
-graphDBEndpoint
-  .update(
-    `insert data {
-      graph <${GRAPHDB_CONTEXT_TEST}> {
-      Testing:Hello rdf:type owl:Class}
-  }`
-         )
-  .then((result) => {
-    console.log("inserted a class :\n" + JSON.stringify(result, null, 2));
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+await login();
 
-// read one class
-graphDBEndpoint
-    .query(
-        `select *from <${GRAPHDB_CONTEXT_TEST}>
-where {
-  ?class rdf:type owl:Class
-  filter(regex(str(?class), "PerspectivesInTrondheim", "i")) .
-}`,
-{ transform: "toJSON" }
-  )
-    .then((result) => {
-        console.log("Read a class:\n" + JSON.stringify(result, null, 2));
-    })
-    .catch((err) => {
-        console.log(err);
-    });
-
-// read all classes
-graphDBEndpoint
-.query(
-    `select ?class from <${GRAPHDB_CONTEXT_TEST}>
-where {
-?class a owl:Class .
-}`,
-{ transform: "toJSON" }
-)
-.then((result) => {
-    console.log("Read all class in graph :\n" + JSON.stringify(result, null, 2));
+app.get('/', async (req, res) => {
+    res.send('hello');
 })
-.catch((err) => {
-    console.log(err);
-});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
