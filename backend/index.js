440c0f70c548056e14b183bbedbad3512201fa9b
AndreasWintherMoen <andreas@winthermoen.no>
2021-02-04 02:44:44 +0100

    Add cors

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,9 @@
 import { login, readClass, readAllClasses, insertClass } from './database.js';
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
+app.use(cors());
 const port = 3000;
 
 await login();
